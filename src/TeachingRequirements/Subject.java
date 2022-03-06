d8b50c8404887c068917edc7a6138c66184ee536
Joe Gilius <2203577g@student.gla.ac.uk>
2022-03-06 19:15:57 +0000

    staff class with required variables

--- src/TeachingRequirements/Subject.java
+++ src/TeachingRequirements/Subject.java
@@ -2,4 +2,9 @@ package TeachingRequirements;
 
 public class Subject {
 
+	private String Name;
+	private int contactNumber;
+	private int ID;
+	private String specialismSubject;
+	private int trainingReq;
 }

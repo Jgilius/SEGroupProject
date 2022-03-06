a5a3de60ad2275695c32a090915f7f1feaeb09e3
Joe Gilius <2203577g@student.gla.ac.uk>
2022-03-06 20:14:44 +0000

    updated all classes and built constructors

---
+++ src/TeachingRequirements/AreaAndSkills.java
@@ -0,0 +1,17 @@
+package TeachingRequirements;
+
+public class AreaAndSkills {
+
+	private String department;
+	private String specialism;
+	private int level; //from a list
+	private int additionalSkills; //from a list
+	
+	public AreaAndSkills(String d, String s, int l, int a) {
+		this.department = d;
+		this.specialism = s;
+		this.level = l;
+		this.additionalSkills = a;
+		
+	}
+}
--- src/TeachingRequirements/Requirements.java
+++ src/TeachingRequirements/Requirements.java
@@ -1,5 +1,10 @@
 package TeachingRequirements;
 
 public class Requirements {
-
+	
+	private int requiredSkills;
+	
+	public Requirements(Staff s, AreaAndSkills a, int r) {
+		
+	}
 }

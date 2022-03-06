8c54d9f4d9361c687904e9e3d22e6084d8438dd1
Joe Gilius <2203577g@student.gla.ac.uk>
2022-03-06 19:17:07 +0000

    updated staff class

--- src/TeachingRequirements/Staff.java
+++ src/TeachingRequirements/Staff.java
@@ -1,5 +1,9 @@
 package TeachingRequirements;
 
 public class Staff {
-
+	private String Name;
+	private int contactNumber;
+	private int ID;
+	private String specialismSubject;
+	private int trainingReq;
 }

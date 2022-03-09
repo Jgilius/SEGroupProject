b483c17f9fff1187b2fc28879e77f33524ed6d0e
Hannah Tallis <2025415t@student.gla.ac.uk>
2022-03-09 20:21:42 +0000

    tidied

--- src/TeachingRequirements/Requirements.java
+++ src/TeachingRequirements/Requirements.java
@@ -15,8 +15,6 @@ public class Requirements {
 	String training;
 	int nStaff = 0;
 	Staff staffList [] = new Staff[20];
-
-	
 	int i;
 	
 	public Requirements () {
@@ -37,7 +35,6 @@ public class Requirements {
 				String sub = tokens[1];
 				String training = tokens[2];
 				staffList[nStaff++] = new Staff(name, sub, training);
-			
 			}	
 		
 		}catch(FileNotFoundException e) {
@@ -54,86 +51,26 @@ public class Requirements {
 			}
 		}
 		
-
-
-			
-			
-			//TODO we need to refactor this for less repeated code - tried to put in a method but then we can't access
-			//the subject that the user types in - SAD :( 
-			
-			//String option = JOptionPane.showInputDialog("Choose (M)aths, (E)nglish or (C)ompSci to see a list of teachers.");
-			
-			//char c = option.charAt(0);
-			
-			//if (c == 'q')
-				//return;
-			
-			//switch(c) {
-			//case 'M' : {
-				System.out.println();
+			System.out.println();
+			if(nStaff>0) {
+				for (i = 0; i<nStaff; i++) {
+					System.out.println("Teachers in the system: " + staffList[i].getName());
+				}
+				String name = JOptionPane.showInputDialog("Check if a teacher requires training by typing their name: ");
+				
 				if(nStaff>0) {
-					for (i = 0; i<nStaff; i++) {
-						System.out.println("Teachers in the system: " + staffList[i].getName());
-					}
-					String name = JOptionPane.showInputDialog("Check if a teacher requires training by typing their name: ");
-					if(nStaff>0) {
 					for (i=0; i <nStaff; i++) {
 						
 						if (name.equals(staffList[i].getName()) && staffList[i].getTraining().equals("no")) {
 							System.out.println(name + " should be booked onto training.");
-				}
+						}
 						else if (name.equals(staffList[i].getName()) && staffList[i].getTraining().equals("yes")) {
 							System.out.println(name + " has already completed their training to teach "  + staffList[i].getSub());
-				
-				
-			}
-			}
-					}
-			
-			
-			
-			/*
-			 * 
-			 * char c = option.charAt(0);
-			if (c == 'q')
-				return;
-				
-			switch(c) {
-			case 'S': {
-				if (subject.equals("maths")) {
-					if(nStaff>0) {
-						for (i = 0; i<nStaff; i++) {
-							if (staffList[i].getSub().equals("maths")) {
-								
-							}
-							
 						}
 					}
-				}
-						
-				else if(subject.equals("english")) {
-					if(nStaff>0) {
-						for (i = 0; i<nStaff; i++) {
-							if (staffList[i].getSub().equals("english")) {
-								System.out.println("Teachers who teach " + subject + ": " + staffList[i].getName());
-							}	
-						}
-				
-					}
-		
 			}
 			
 		}
-	}
-	
-	}
-	
-		*/	
-		}
-				
-	
-				}
-				
-	}
-
-
+					
+		}			
+}
